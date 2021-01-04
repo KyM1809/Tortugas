31783ff86643feba506dc0642b8451fb7f6e45ce
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-04 16:30:16 -0600

    Create CerrarSesion.php

---
+++ php/CerrarSesion.php
@@ -0,0 +1,11 @@
+<?php
+	session_start();
+	//include "Utiles.php";
+	//$Util = new Utiles();
+	//$Util->ActualizarDatosCierre($_SESSION["Usuario"]);
+	session_unset("Logueado");
+	session_unset("Usuario");
+	session_unset("Nombre");
+	session_destroy();
+	header("Location:../index.php");
+?>
