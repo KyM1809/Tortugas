f8122f34aa80ab43c5e51fac07bf668d69aee882
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-04 16:30:21 -0600

    Create Header.php

---
+++ php/Header.php
@@ -0,0 +1,40 @@
+<div class="row">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light" style="width: 100%; margin: 0;">
+					<a class="navbar-brand" href="#">Karen's Boutique</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="VenderProductos.php">Punto de ventas<span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Lista de productos
+								</a>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="ListaProductos.php">Boutique</a>
+									<a class="dropdown-item" href="ListaProductosCafe.php">Cafe</a>
+								</div>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Registrar productos
+								</a>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="RegistrarProductos.php">Boutique</a>
+									<a class="dropdown-item" href="RegistrarProductosCafe.php">Cafe</a>
+								</div>
+							</li>
+							<li class="nav-item active">
+								<a class="nav-link" href="Ventas.php">Ventas<span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item active">
+								<a class="nav-link" href="php/CerrarSesion.php">Cerrar sesion<span class="sr-only">(current)</span></a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
