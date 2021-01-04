c81da1f6780c418f789befa6ecc3042f2fe8c050
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-04 16:30:30 -0600

    Create Scripts.php

---
+++ php/Scripts.php
@@ -0,0 +1,10 @@
+<?php
+	echo '<script type="text/javascript" src="JS/jquery-3.4.1.min.js"></script>
+	<script type="text/javascript" src="Bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="JS/jquery.toast.js"></script>
+	<script type="text/javascript" src="JS/jquery.table2excel.js"></script>
+	<script type="text/javascript" src="JS/gijgo.min.js"></script>
+	<script type="text/javascript" src="JS/popper-1.14.0.min.js"></script>
+	<script type="text/javascript" src="JS/solid.js"></script>
+';
+?>
