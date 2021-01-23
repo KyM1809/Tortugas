c36d45a1675141357a52c5146d1e7c030eddc00c
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-23 09:08:46 -0600

    Update Publicaciones.php

--- Publicaciones.php
+++ Publicaciones.php
@@ -325,6 +325,16 @@
 					</nav>
 				</div>
 			</div>
+			<div class="row">
+				<div class="col-12">
+					<form>
+						<div class="row">
+							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-3"></div>
+							<div class="col-xs-12 col-sm-12 col-md-4 col-lg-3"></div>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
