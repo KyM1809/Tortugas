bf3250a9d7ce11a41baaebae9e09fb7f3a5a50a1
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-22 22:48:41 -0600

    Update CerrarSesion.php

--- php/CerrarSesion.php
+++ php/CerrarSesion.php
@@ -3,6 +3,7 @@
 	//include "Utiles.php";
 	//$Util = new Utiles();
 	//$Util->ActualizarDatosCierre($_SESSION["Usuario"]);
+	$_SESSION["Logueado"] = false;
 	session_unset("Logueado");
 	session_unset("Usuario");
 	session_unset("Nombre");
