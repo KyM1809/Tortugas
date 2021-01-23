97c124108f4deb1ce924ccfd960c21bf0fff08ad
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-23 08:22:21 -0600

    Update Meta.php

--- php/Meta.php
+++ php/Meta.php
@@ -1,5 +1,5 @@
 <?php
-	echo '<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+	echo '<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8" charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE-Edge" charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE7" charset="utf-8">
