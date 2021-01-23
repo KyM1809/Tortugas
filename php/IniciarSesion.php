6181b4ea8be9b7acd17339600f38ce70b149cd21
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-22 22:38:03 -0600

    Update IniciarSesion.php

--- php/IniciarSesion.php
+++ php/IniciarSesion.php
@@ -29,17 +29,18 @@
 		public function Iniciar(){
 			if($this->Util->ComprobarExisteUsuario($_POST["Usuario"], hash('sha512', $_POST["Contrasena"])) == "1"){
 				$this->MyConnection = $this->Connection->Conectar();
-				$this->Consulta = "CALL `SP006ObtenerDatosUsuario`(?,?);";
+				$this->Consulta = "CALL `SP03ObtenerDatosUsuario`(?,?);";
 				if( !$this->Solicitud = $this->MyConnection->prepare( $this->Consulta ) ){
-					//
+					echo '{}';
 				}
-
-				if( !$this->Solicitud->bind_param("ss", $_POST["Usuario"], hash('sha512', $_POST["Contrasena"]))){
-					//
+				$u = $_POST["Usuario"];
+				$c = hash('sha512', $_POST["Contrasena"]);
+				if( !$this->Solicitud->bind_param("ss", $u, $c)){
+					echo '{}';
 				}
 
 				if( !$this->Solicitud->execute() ){
-					//
+					echo '{}';
 				}else{
 					$this->Resultado = $this->Solicitud->get_result();
 					$this->Respuesta = $this->Resultado->fetch_assoc();
@@ -49,7 +50,7 @@
 					$_SESSION["Tipo"] = $this->Respuesta["TipoUsuario"];
 					$_SESSION["Logueado"] = true;
 
-					header('location:Inicio.php');
+					header('location:../index.php');
 				}
 			}else{
 				header('location:Inicio.php');
