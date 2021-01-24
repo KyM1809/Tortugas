7cba1477dc642ae5ce0e74d7d556f631f50709ce
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-24 00:43:59 -0600

    Update bdcamptort_d6wn4.sql

--- bdcamptort_d6wn4.sql
+++ bdcamptort_d6wn4.sql
@@ -7,13 +7,12 @@
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
 -- Volcando estructura de base de datos para bdcamptort_d6wn4
-CREATE DATABASE IF NOT EXISTS `bdcamptort_d6wn4` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `bdcamptort_d6wn4` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `bdcamptort_d6wn4`;
 
 -- Volcando estructura para procedimiento bdcamptort_d6wn4.SP01RegistrarUsuario
