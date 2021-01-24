284ff04eaab7e8fdf70e46ed2f410f8ea40dd5f8
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-24 03:50:07 -0600

    Update Nido.php

--- php/Nido.php
+++ php/Nido.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	include "Utiles.php";
-	class Sesion {
+	class Nido {
 		private $Consulta;
 		private $Resultado;
 		private $Connection;
@@ -26,39 +26,44 @@
 			$this->Respuesta = "";
 		}
 
-		public function Iniciar(){
-			if($this->Util->ComprobarExisteUsuario($_POST["Usuario"], hash('sha512', $_POST["Contrasena"])) == "1"){
-				$this->MyConnection = $this->Connection->Conectar();
-				$this->Consulta = "CALL `SP03ObtenerDatosUsuario`(?,?);";
-				if( !$this->Solicitud = $this->MyConnection->prepare( $this->Consulta ) ){
-					echo '{}';
-				}
-				$u = $_POST["Usuario"];
-				$c = hash('sha512', $_POST["Contrasena"]);
-				if( !$this->Solicitud->bind_param("ss", $u, $c)){
-					echo '{}';
-				}
+		public function Crear(){
+			$this->MyConnection = $this->Connection->Conectar();
+			$this->Consulta = "CALL `SP04CrearNido`(?,?);";
+			if( !$this->Solicitud = $this->MyConnection->prepare( $this->Consulta ) ){
+				echo '{}';
+			}
+			$nom = $_POST["NombreNido"];
+			$num = $_POST["NumeroHuevos"];
+			if( !$this->Solicitud->bind_param("si", $nom, $num)){
+				echo '{}';
+			}
 
-				if( !$this->Solicitud->execute() ){
-					echo '{}';
-				}else{
-					$this->Resultado = $this->Solicitud->get_result();
-					$this->Respuesta = $this->Resultado->fetch_assoc();
-					
-					$_SESSION["Usuario"] = $this->Respuesta["Usuario"];
-					$_SESSION["Nombre"] = $this->Respuesta["Nombre"];
-					$_SESSION["Tipo"] = $this->Respuesta["TipoUsuario"];
-					$_SESSION["Logueado"] = true;
-					#print_r($this->Respuesta);
-					#print_r($_SESSION);
-					header('location:../index.php');
-				}
+			if( !$this->Solicitud->execute() ){
+				echo '{}';
 			}else{
-				header('location:Inicio.php');
+				header('location:../Nidos.php');
 			}
 		}
 	}
 
-	$Obj = new Sesion();
-	$Obj->Iniciar();
+	$Obj = new Nido();
+	if(isset($_POST['Id'])){
+		switch ($_POST['Id']) {
+			case '1':
+				$Obj->Crear();
+				break;
+
+			case '2':
+				# code...
+				break;
+
+			case '3':
+				# code...
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+	}
 ?>
