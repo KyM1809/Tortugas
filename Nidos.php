6f546ca4b8bf9622134934d60d128eb5790ae801
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-01-24 00:42:41 -0600

    Update Nidos.php

--- Nidos.php
+++ Nidos.php
@@ -1,6 +1,16 @@
 <?php
 	session_start();
-	if(!isset($_SESSION["Logueado"])){
+	if(isset($_SESSION["Logueado"])){
+		if($_SESSION['Logueado']){
+			if($_SESSION['Tipo'] == '1'){
+				#
+			}else{
+				header('location:Inicio.php');
+			}
+		}else{
+			header('location:Inicio.php');
+		}
+	}else{
 		header('location:Inicio.php');
 	}
 ?>
