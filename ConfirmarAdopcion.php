0e21eceef0dfc140193a1ba6428f671f0c9d74d5
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-02-02 12:41:04 -0600

    Update ConfirmarAdopcion.php

--- ConfirmarAdopcion.php
+++ ConfirmarAdopcion.php
@@ -9,8 +9,8 @@
 	<head>
 		<title>Confirmar adopción</title>
 		<?php
-			include('php/meta.php');
-			include('php/styles.php');
+			include('php/Meta.php');
+			include('php/Styles.php');
 		?>
 		<style type="text/css">
 			@media screen and (max-width: 800px) {
