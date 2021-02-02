2b09fdaaf3b10ad6a0ab20e88e470ffdea54f19f
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-02-02 00:09:36 -0600

    Create Publicacion.php

---
+++ php/Publicacion.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+
+	class Publicacion{
+		private $Consulta;
+		private $Resultado;
+		private $Connection;
+		private $JsonData;
+		private $MyConnection;
+		private $Solicitud;
+		private $Respuesta;
+		private $Numero;
+		private $Util;
+
+		public function __construct(){
+			$this->Consulta = "";
+			$this->Connection = new ConexionClass();
+			$this->Solicitud = "";
+			$this->Respuesta = "";
+			$this->Numero = "3";
+			$this->Numero1 = "4";
+			$this->Util = new Utiles();
+
+			$this->Consulta = "";
+			$this->Solicitud = "";
+			$this->Respuesta = "";
+		}
+	}
+?>
