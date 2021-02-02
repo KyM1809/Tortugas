7fefa40a9935f16ac08624886bd51fd537d6691f
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-02-02 01:04:15 -0600

    Update Publicacion.php

--- php/Publicacion.php
+++ php/Publicacion.php
@@ -27,12 +27,38 @@
 		}
 
 		public function Publicar(){
-			#
+			$this->MyConnection = $this->Connection->Conectar();
+			$this->Consulta = "CALL `SP04CrearNido`(?,?);";
+			//$this->Consulta = "INSERT INTO tnidos(Nido, Huevos, Adoptado, Adopta) VALUES(?,?, 2, Null);";
+			if( !$this->Solicitud = $this->MyConnection->prepare( $this->Consulta ) ){
+				echo '{}';
+			}
+			$nom = $_POST["NombreNido"];
+			$num = $_POST["NumeroHuevos"];
+			if( !$this->Solicitud->bind_param("si", $nom, $num)){
+				echo '{}';
+			}
+
+			if( !$this->Solicitud->execute() ){
+				echo '{}';
+			}else{
+				header('location:../Nidos.php');
+				//echo '<br><b>ID:</b>' . $this->Connection->lastInsertId() . '<br><br><br>';
+				//echo '<br><b>ID:</b>' . $this->Connection->mysql_insert_id . '<br><br><br>';
+				//echo '<br><b>ID:</b>' . $this->Connection->insert_id . '<br><br><br>';
+				#########
+				#		#
+				#	OK	#
+				#		#
+				#########
+				//print_r($this->Solicitud);
+				//echo '<br><b>ID:</b>' . $this->Solicitud->insert_id . '<br><br><br>';
+			}
 		}
 
 		public function ListarPublicaciones(){
 			#
 		}
-		
+
 	}
 ?>
