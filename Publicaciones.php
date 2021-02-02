9437fabad6a384fa708da343b0509628904c40ee
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2021-02-02 12:41:22 -0600

    Update Publicaciones.php

--- Publicaciones.php
+++ Publicaciones.php
@@ -10,8 +10,8 @@
 	<head>
 		<title>Publicaciones</title>
 		<?php
-			include('php/meta.php');
-			include('php/styles.php');
+			include('php/Meta.php');
+			include('php/Styles.php');
 		?>
 		<style type="text/css">
 			@media screen and (max-width: 800px) {
